2337218e525e60ea37a160d121e723375fa488ba
ismail0vv <asylismailov05@gmail.com>
2023-06-12 23:45:34 +0600

    fix

--- app/tour/models.py
+++ app/tour/models.py
@@ -2,11 +2,22 @@ from django.db import models
 
 
 class TourAdd(models.Model):
+    HORSE_TOUR = '1'
+    WALK_TOUR = '2'
+    JEEP_TOUR = '3'
+    WINTER_TOUR = '4'
+    TYPE_CHOICES = (
+        (HORSE_TOUR, 'Конный тур'),
+        (WALK_TOUR, 'Пеший тур'),
+        (JEEP_TOUR, 'Джип тур'),
+        (WINTER_TOUR, 'Зимний тур'),
+    )
     name = models.CharField(max_length=100)
     tour_time = models.CharField(max_length=100)
     number_of_people = models.IntegerField()
     price = models.IntegerField()
     when_is_tour = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, verbose_name="Тип тура", null=True, blank=False)
     image = models.ImageField(upload_to='tour_images')
     image_2 = models.ImageField(upload_to='tour_images', blank=True, null=True)
     image_3 = models.ImageField(upload_to='tour_images', blank=True, null=True)
@@ -179,21 +190,10 @@ class BookingGroupTour(models.Model):
 
 
 class BookingPrivateTour(models.Model):
-    HORSE_TOUR = '1'
-    WALK_TOUR = '2'
-    JEEP_TOUR = '3'
-    WINTER_TOUR = '4'
-    TYPE_CHOICES = (
-        (HORSE_TOUR, 'Конный тур'),
-        (WALK_TOUR, 'Пеший тур'),
-        (JEEP_TOUR, 'Джип тур'),
-        (WINTER_TOUR, 'Зимний тур'),
-    )
     name = models.CharField(max_length=100, blank=False, null=True, verbose_name="Имя: ")
     email_or_whatsapp = models.CharField(max_length=100, blank=False, null=True, verbose_name="Контакты: ")
     date = models.DateTimeField(verbose_name="Дата от: ")
     date_up_to = models.DateTimeField(verbose_name="Дата до: ")
-    type = models.CharField(max_length=100, verbose_name="Тип тура", null=True, blank=False)
     tour = models.ForeignKey(TourAdd, on_delete=models.CASCADE, related_name="private_bookings")
 
     def __str__(self):
